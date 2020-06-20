683e05df20e4245d00a0eb38fa865c736691c0b3
jieyi <pokkbaby@gmail.com>
2020-06-20 14:43:07 +0900

    build(firebase): bump into the newest version

--- buildSrc/src/main/java/config/GradleDependency.kt
+++ buildSrc/src/main/java/config/GradleDependency.kt
@@ -30,7 +30,7 @@ object GradleDependency {
     object Version {
         const val SAFE_ARGS = NAVIGATION_KTX
         const val GOOGLE_SERVICE = "4.3.3"
-        const val CRASHLYTICS = "2.1.1"
+        const val CRASHLYTICS = "2.2.0"
         const val DETEKT = "1.9.1"
         const val VERSION_UPDATER = "0.28.0"
     }
