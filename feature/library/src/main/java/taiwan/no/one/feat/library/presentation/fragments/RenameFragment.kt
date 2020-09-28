ca80212228f309742460625a1c6a6c180d5742f7
jieyi <pokkbaby@gmail.com>
2020-09-28 17:25:34 +0900

    feat(*): add the renaming fragment for the view xml and the controller

---
+++ feature/library/src/main/java/taiwan/no/one/feat/library/presentation/fragments/RenameFragment.kt
@@ -0,0 +1,37 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Jieyi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package taiwan.no.one.feat.library.presentation.fragments
+
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
+import taiwan.no.one.core.presentation.activity.BaseActivity
+import taiwan.no.one.core.presentation.fragment.BaseFragment
+import taiwan.no.one.feat.library.databinding.FragmentRenameBinding
+import taiwan.no.one.feat.library.presentation.viewmodels.PlaylistViewModel
+
+internal class RenameFragment : BaseFragment<BaseActivity<*>, FragmentRenameBinding>() {
+    private val vm by viewModels<PlaylistViewModel>()
+    private val navArgs by navArgs<RenameFragmentArgs>()
+}
