376ac493e19e200706d531d7857b55f4c8cc4871
jieyi <pokkbaby@gmail.com>
2020-10-08 22:24:47 +0900

    test(*): add the unit test tools

--- test/src/test/java/taiwan/no/one/test/Kits.kt
+++ test/src/test/java/taiwan/no/one/test/Kits.kt
@@ -24,18 +24,9 @@
 
 package taiwan.no.one.test
 
-import org.junit.Test
-
-import org.junit.Assert.*
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
-    }
+object Kits {
+    fun openJsonFile(path: String) = javaClass.classLoader
+        ?.getResourceAsStream(path)
+        ?.bufferedReader()
+        ?.use { it.readText() }
 }
