2fd63dc1e2d4f7f15d0ee70421030b29694dc11e
jieyi <pokkbaby@gmail.com>
2021-02-09 00:24:56 +0900

    build(*): bump into the pre version gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ buildscript {
         }
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.2")
+        classpath("com.android.tools.build:gradle:7.0.0-alpha05")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
         classpath(config.GradleDependency.KOTLIN)
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -41,7 +41,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:4.1.2")
+    implementation("com.android.tools.build:gradle:7.0.0-alpha05")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
 
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.15.0")
