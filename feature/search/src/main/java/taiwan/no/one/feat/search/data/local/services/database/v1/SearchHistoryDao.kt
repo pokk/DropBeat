aa5ff441b7e30be8f8af6cd6fe3edd9a939f6497
jieyi <pokkbaby@gmail.com>
2021-05-05 18:18:12 +0900

    fix(search): change the search icon's color

--- feature/search/src/main/java/taiwan/no/one/feat/search/data/local/services/database/v1/SearchHistoryDao.kt
+++ feature/search/src/main/java/taiwan/no/one/feat/search/data/local/services/database/v1/SearchHistoryDao.kt
@@ -27,10 +27,10 @@ package taiwan.no.one.feat.search.data.local.services.database.v1
 import androidx.room.Dao
 import androidx.room.Query
 import androidx.room.Transaction
+import java.util.Date
 import kotlinx.coroutines.flow.Flow
 import taiwan.no.one.core.data.local.room.BaseDao
 import taiwan.no.one.feat.search.data.entities.local.SearchHistoryEntity
-import java.util.Date
 
 /**
  * Integrated the base [androidx.room.Room] database operations.
