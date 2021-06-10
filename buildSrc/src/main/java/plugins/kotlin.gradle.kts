64ec8e87203b17ec453194e4490a5311ee7507a6
jieyi <pokkbaby@gmail.com>
2021-06-10 19:29:31 +0900

    fix(*): fix the result issue

--- buildSrc/src/main/java/plugins/kotlin.gradle.kts
+++ buildSrc/src/main/java/plugins/kotlin.gradle.kts
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2020 Jieyi
+ * Copyright (c) 2021 Jieyi
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -34,6 +34,7 @@ subprojects {
             freeCompilerArgs = listOf(
                 "-Xopt-in=kotlin.RequiresOptIn",
                 "-Xopt-in=kotlin.ExperimentalStdlibApi",
+                "-Xallow-result-return-type"
             )
         }
     }
