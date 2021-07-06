c9af29a1b2db58ab993380c5c4dbb00ebd6366bb
jieyi <pokkbaby@gmail.com>
2021-07-07 01:01:42 +0900

    feat(*): add save file into local storage

--- app/src/main/java/taiwan/no/one/dropbeat/core/helpers/DownloadHelper.kt
+++ app/src/main/java/taiwan/no/one/dropbeat/core/helpers/DownloadHelper.kt
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2020 Jieyi
+ * Copyright (c) 2021 Jieyi
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -29,6 +29,13 @@ import android.app.DownloadManager.Request
 import android.content.Context
 import android.net.Uri
 import android.os.Environment
+import android.util.Log
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+import okhttp3.ResponseBody
 
 object DownloadHelper {
     fun downloadTrack(context: Context, uri: Uri, filename: String, songEntityStream: String) {
@@ -40,5 +47,45 @@ object DownloadHelper {
             setDescription(songEntityStream)
         }
         downloadManager.enqueue(request)
+    }
+
+    private fun writeResponseBodyToDisk(context: Context, body: ResponseBody): Boolean {
+        return try {
+            // todo change the file location/name according to your needs
+            val futureStudioIconFile =
+                File(
+                    context.getExternalFilesDir(null).toString() + File.separator.toString() + "Future Studio Icon.png"
+                )
+            var inputStream: InputStream? = null
+            var outputStream: OutputStream? = null
+            try {
+                val fileReader = ByteArray(4096)
+                val fileSize: Long = body.contentLength()
+                var fileSizeDownloaded: Long = 0
+                inputStream = body.byteStream()
+                outputStream = FileOutputStream(futureStudioIconFile)
+                while (true) {
+                    val read: Int = inputStream.read(fileReader)
+                    if (read == -1) {
+                        break
+                    }
+                    outputStream.write(fileReader, 0, read)
+                    fileSizeDownloaded += read.toLong()
+                    Log.d("TAGGGGG", "file download: $fileSizeDownloaded of $fileSize")
+                }
+                outputStream.flush()
+                true
+            }
+            catch (e: IOException) {
+                false
+            }
+            finally {
+                inputStream?.close()
+                outputStream?.close()
+            }
+        }
+        catch (e: IOException) {
+            false
+        }
     }
 }
