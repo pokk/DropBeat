0a44043e091fc697f4337eebde235efa33977a47
jieyi <pokkbaby@gmail.com>
2021-08-25 18:55:37 +0900

    build(*): bump kotlin version into 1.5.30

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -37,7 +37,7 @@ repositories {
 
 dependencies {
     implementation("com.android.tools.build:gradle:7.1.0-alpha09")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30-RC")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
 
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.18.0")
     implementation("com.github.ben-manes:gradle-versions-plugin:0.39.0")
